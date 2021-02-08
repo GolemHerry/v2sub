bed3da97a60d736413bbf3564b43821f7cb6faec
GolemHerry <forsakenhale@gmail.com>
2021-02-08 16:59:21 +0800

    Add subscribe Info

--- pkg/subscribe/subscribe.go
+++ pkg/subscribe/subscribe.go
@@ -1,12 +1,27 @@
 package subscribe
 
 import (
+	"bufio"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
 
 const rawFilePath = "/.v2sub/raw.txt"
+
+type userInfo struct {
+	Remark   string      `json:"remark"`
+	Type     interface{} `json:"type"`
+	Addition string      `json:"add"`
+	Id       string      `json:"id"`
+	Net      string      `json:"net"`
+	AlterId  int         `json:"alterId"`
+	Ps       string      `json:"ps"`
+}
 
 func Update(url string) error {
 	client := http.Client{}
@@ -39,4 +54,40 @@ func Update(url string) error {
 	}
 
 	return nil
+}
+
+func Info() error {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	rawFile, err := os.Open(homedir + rawFilePath)
+	if err != nil {
+		panic(err)
+	}
+	defer rawFile.Close()
+
+	decodeReader := base64.NewDecoder(base64.StdEncoding, rawFile)
+	dest, err := bufio.NewReader(decodeReader).ReadBytes('\n')
+	if err != nil {
+		panic(err)
+	}
+
+	if len(dest) < 8 || string(dest[:5]) != "vmess" {
+		return errors.New("Invalid protocol")
+	}
+
+	var info userInfo
+	if err := json.Unmarshal(dest[8:], &info); err != nil {
+		return err
+	}
+	printInfo(info)
+
+	return nil
+}
+
+func printInfo(info userInfo) {
+	fmt.Printf("User info: \n%#v\n", info)
+	//TODO beautify output
 }
